4f941b235606eeac904dfae9003454cc9223bf16
Himanshu Agrawal <Himanshuagrawal7766@gmail.com>
2025-10-06 18:18:06 +0530

    Add Deleting key

--- Development/src/TodoList.jsx
+++ Development/src/TodoList.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todo, setTodo] = useState([{task: "Sample Task", id: uuidv4()}]);
+  let [todo, setTodo] = useState([{ task: "Sample Task", id: uuidv4() }]);
   let [newTodo, setNewTodo] = useState("");
 
   let addButton = () => {
-    setTodo([...todo, {task: newTodo, id: uuidv4()}]);
+    setTodo([...todo, { task: newTodo, id: uuidv4() }]);
     setNewTodo(" ");
   };
 
   let updateTodoValue = (event) => {
     setNewTodo(event.target.value);
   };
+
+  let deleteTodo = (id) =>{
+    setTodo(todo.filter((todo)=>todo.id != id));
+  };
+
   return (
     <>
-      <input 
+      <input
         placeholder="...Add task"
         value={newTodo}
         onChange={updateTodoValue}
@@ -29,7 +34,11 @@ export default function TodoList() {
       <h4>Todo List</h4>
       <ul>
         {todo.map((todo) => (
-          <li key={todo.id}>{todo.task}</li>
+          <li key={todo.id}>
+            <span>{todo.task}</span>
+            &nbsp; &nbsp; &nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </>
