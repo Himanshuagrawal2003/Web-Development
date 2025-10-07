c99cc9332299c14422e5677d0c1486f548150a6c
Himanshu Agrawal <Himanshuagrawal7766@gmail.com>
2025-10-07 21:22:55 +0530

    Add Updating All Element in Array

--- Development/src/TodoList.jsx
+++ Development/src/TodoList.jsx
@@ -2,20 +2,32 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todo, setTodo] = useState([{ task: "Sample Task", id: uuidv4() }]);
+  let [todo, setTodo] = useState([{ task: "Sample Task", id: uuidv4() }]); //Define objects in array
   let [newTodo, setNewTodo] = useState("");
 
   let addButton = () => {
-    setTodo([...todo, { task: newTodo, id: uuidv4() }]);
+    setTodo([...todo, { task: newTodo, id: uuidv4() }]); //Spread todo
     setNewTodo(" ");
   };
 
   let updateTodoValue = (event) => {
+    //event object use only targent property
     setNewTodo(event.target.value);
   };
 
-  let deleteTodo = (id) =>{
-    setTodo(todo.filter((todo)=>todo.id != id));
+  let deleteTodo = (id) => {
+    setTodo((prevTodo) => todo.filter((prevTodo) => prevTodo.id != id)); // callback function and Also filter fuction to filter todo list
+  };
+
+  let upperCaseAll = () => {
+    setTodo((prevTask) =>
+      prevTask.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      })
+    );
   };
 
   return (
@@ -33,14 +45,20 @@ export default function TodoList() {
       <hr></hr>
       <h4>Todo List</h4>
       <ul>
-        {todo.map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.task}</span>
-            &nbsp; &nbsp; &nbsp;
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
+        {todo.map(
+          (
+            todo //map function for render
+          ) => (
+            <li key={todo.id}>
+              <span>{todo.task}</span>
+              &nbsp; &nbsp; &nbsp;
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+          )
+        )}
       </ul>
+      <br></br>
+      <button onClick={upperCaseAll}>UpperCaseAll</button>
     </>
   );
 }
