334407934e65ddf627d07f61708fe4e8e30b8e96
Himanshu Agrawal <Himanshuagrawal7766@gmail.com>
2025-10-10 18:29:06 +0530

    Handling Multiple Inputs in Function And UserVariables

--- Development/src/Form.jsx
+++ Development/src/Form.jsx
@@ -1,17 +1,64 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-export default function Form(){
-    let [formName, setFormName] = useState("");
+export default function Form() {
+  let [formData, setFormData] = useState({
+    fullName:"",
+    username:"",
+    password:""
+  });
 
-    let handleNameChange = (event) => {
-        setFormName(event.target.value);
-    };
+  let handleInputChange = (event) => {
+    setFormData ((currData) => {
+      return {...currData, [event.target.name]:event.target.value};
+    });
+  };
 
-    return (
-    <form>
-        <label htmlFor='username'>Full Name  </label>                    {/* in react use htmlFor for connect input*/}
-        <input type="text" id='username' placeholder="Enter Your Name" onChange={handleNameChange} value={formName} />
-        <button>Submit</button>
+  let handleSubmit = (event) =>{
+    event.preventDefault();
+    console.log(formData);
+    setFormData({
+      fullName:"",
+      username:"",
+      password:""
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="fullname">Full Name </label>{" "}
+      {/* in react use htmlFor for connect input*/}
+      <input
+        type="text"
+        id="fullname"
+        placeholder="Enter Your Full Name"
+        onChange={handleInputChange}
+        value={formData.fullName}
+        name="fullName"
+      />
+      <br></br><br></br>
+      <label htmlFor="username">User Name </label>{" "}
+      {/* in react use htmlFor for connect input*/}
+      <input
+        type="text"
+        id="username"
+        placeholder="Enter Your UserName"
+        onChange={handleInputChange}
+        value={formData.username}
+        name="username"
+      />
+      <br></br><br></br>
+       <label htmlFor="password">PassWord </label>{" "}
+      {/* in react use htmlFor for connect input*/}
+      <input
+        type="password"
+        id="password"
+        placeholder="Password"
+        onChange={handleInputChange}
+        value={formData.password}
+        name="password"
+      />
+      <br></br><br></br>
+      <button>Submit</button>
     </form>
-    );
+  );
 }
